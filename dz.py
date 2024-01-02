430229b4b5cda0a663c59721f10956e000c046c6
Dima0913 <dimarovno2009@gmail.com>
2024-01-02 13:04:13 +0200

    first commit

---
+++ dz.py
@@ -0,0 +1,56 @@
+# Ex2
+# import random
+# list = []
+# count = 0 
+# count1 = 0
+# count2 = 0
+# for i in range(20):
+#     list.append(random.randint(-99,99))
+#     print(list[i])
+# min= list[0]
+# max = list[0]
+# for i in list:
+#     if max < i:
+#         max = i
+#     elif min > i:
+#         min = i
+# print(f'Max = {max}')
+# print(f"Min = {min}")
+# for i in list:
+#     if i < 0:
+#         count += 1
+#     elif i > 0:
+#         count1 += 1
+#     elif i == 0:
+#         count2 += 1
+# print(count)
+# print(count1)
+# print(count2)
+
+
+        
+
+# Ex1
+# num1 = int(input("Enter first num : "))
+# znak = input("Enter znak : ")
+# num2 = int(input("enter second num : "))
+# if znak == "+":
+#     print(num1 + num2)
+# elif znak == "-":
+#     print(num1-num2)
+# elif znak == "*":
+#     print(num1 * num2)
+# elif znak == "/":
+#     if(num2 != 0):
+#         print(num1/num2)
+#     else:
+#         print("I can not devide with 0!!!")
+# else:
+#     print("Something went wrong...")    
+
+
+
+
+        
+    
+     
